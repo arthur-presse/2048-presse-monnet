be318dc9842e6453486937d815481d6eba66fe6d
Arnaud Monnet <monnet.arnaud@laposte.net>
2020-04-08 16:07:57 +0200

    Ajout d'un widget personnalisé GameWidget

---
+++ gamewidget.h
@@ -0,0 +1,16 @@
+#ifndef GAMEWIDGET_H
+#define GAMEWIDGET_H
+
+#include <QWidget>
+
+class GameWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit GameWidget(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GAMEWIDGET_H
