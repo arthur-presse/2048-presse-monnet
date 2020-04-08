f0edea8367082e4ec0c1686b804d94ed9bf70fdd
arthur-presse <arthurpresse@hotmail.fr>
2020-04-08 18:08:02 +0200

    Ajout des différentes sous-classes pour le mouvement (droite, gauche, bas, droite) héritant de la classe Move

--- move.h
+++ move.h
@@ -8,11 +8,11 @@
 class Move
 {
 protected:
-    typedef int (Move::* )(Case *grid)
+    typedef int (Move::* deep)(Case *grid);
 
     int x;
 
-    virtual int getclose(int x) = 0;
+    virtual int getcloser(int x) = 0;
     virtual bool next(void) = 0;
     virtual void init(void) = 0;
     virtual Move::deep getfusion(void) = 0;
@@ -25,5 +25,41 @@ public:
     bool move(Case *grille, int &val, bool anim = true);
 
 };
+//---Création de chacun des déplacements---
 
+//mouvement vers le haut
+class MoveHigh : public Move {
+protected:
+    virtual int getcloser(int x);
+    virtual bool next(void);
+    virtual void init(void);
+    virtual Move::deep getfusion(void);
+};
+
+//mouvement vers le bas
+class MoveDown : public Move {
+protected:
+    virtual int getcloser(int x);
+    virtual bool next(void);
+    virtual void init(void);
+    virtual Move::deep getfusion(void);
+};
+
+//mouvement vers la gauche
+class MoveLeft : public Move {
+protected:
+    virtual int getcloser(int x);
+    virtual bool next(void);
+    virtual void init(void);
+    virtual Move::deep getfusion(void);
+};
+
+//mouvement vers la droite
+class MoveRight : public Move {
+protected:
+    virtual int getcloser(int x);
+    virtual bool next(void);
+    virtual void init(void);
+    virtual Move::deep getfusion(void);
+};
 #endif // MOVE_H
