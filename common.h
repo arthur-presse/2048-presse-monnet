7832f16a1445fdf63316c3f17819355bd640881e
arthur-presse <arthurpresse@hotmail.fr>
2020-04-08 18:27:03 +0200

    petites modifs

--- common.h
+++ common.h
@@ -4,15 +4,14 @@
 #define COTE    4
 #define NbCase  (COTE^2)
 
-typedef struct Case {
+typedef struct {
     //value est la valeur de la case de la grille
     int value;
     // new et fusion servent à l'affichage graphique du jeu
     bool newvalue;
     bool fusion;
-
-}
-//
+}Case;
+//définition de la grille que nous allons utiliser pour stocker toutes les valeurs durant une partie
 typedef Case Grid[NbCase];
 
 #endif // COMMON_H
