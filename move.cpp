308fd889c5cb563ffec10f3d8a06799daf931cbb
arthur-presse <arthurpresse@hotmail.fr>
2020-04-08 17:35:20 +0200

    Début de création des différentes fonctions de déplacements

--- move.cpp
+++ move.cpp
@@ -3,20 +3,22 @@
 #include "move.h"
 
 
-int Move::Fusion_Possible_par_Ligne(SCase *grille){
+int Move::Fusion_Possible_par_Ligne(Case *grid){
+    //fonction pour compter le nombre de fusions possibles pour un mouvement en ligne nb : quelque soit le sens, droit ou gauche, le nombre de fusions
+    //pour un mouvemnt en colonne est le même
     int l, c;
     int res = 0;
     for (l=0;l<COTE;l++){
-        int temp = grille[l].valeur;
+        int temp = grid[l].valeur;
         for(c=1;c<COTE;c++) {
             int x=c*COTE+l;
-            if (grille[x].valeur == temp) {
+            if (grid[x].valeur == temp) {
                 res=res+1;
                 temp = 0;
             }
             else
             {
-                temp = grille[x].valeur
+                temp = grid[x].valeur
             }
 
         }
@@ -25,3 +27,49 @@ int Move::Fusion_Possible_par_Ligne(SCase *grille){
     return res;
 
 }
+
+
+int Move::Fusion_Possible_par_Colonne(Case *grid){
+    //fonction pour compter le nombre de fusions possibles pour un mouvement en colonne nb : quelque soit le sens, haut ou bas, le nombre de fusions
+    //pour un mouvemnt en colonne est le même
+    int l, c;
+    int res = 0;
+    for (c=0;c<COTE;c++){
+        int temp = grid[COTE*c].valeur;
+        for(l=1;l<COTE;l++) {
+            int x=c*COTE+l;
+            if (grid[x].valeur == temp) {
+                res=res+1;
+                temp = 0;
+            }
+            else
+            {
+                temp = grid[x].valeur
+            }
+
+        }
+    }
+
+    return res;
+
+}
+
+
+bool Move::move(Case *grid, int &val, bool anim){
+
+    int nbMove = 0;
+    bool moved;
+
+    do {
+        moved = false;
+
+        do {
+
+        }
+    }
+
+    if(grid[voisin].valeur == grid)
+
+
+}
+
--- move.h
+++ move.h
@@ -7,8 +7,23 @@
 
 class Move
 {
+protected:
+    typedef int (Move::* )(Case *grid)
+
+    int x;
+
+    virtual int getclose(int x) = 0;
+    virtual bool next(void) = 0;
+    virtual void init(void) = 0;
+    virtual Move::deep getfusion(void) = 0;
+    int Fusion_Possible_par_Ligne(Case *grid);
+    int Fusion_Possible_par_Colonne(Case *grid);
+
+
+
 public:
-    Move();
+    bool move(Case *grille, int &val, bool anim = true);
+
 };
 
 #endif // MOVE_H
