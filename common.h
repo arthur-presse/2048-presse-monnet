557f6d02439c2e7c9287fe1ffff0c4dc6097e8d3
arthur-presse <arthurpresse@hotmail.fr>
2020-04-08 18:17:24 +0200

    Création du type de la grille que nous allons utiliser pour stocker les valeurs pendant la partie

--- common.h
+++ common.h
@@ -4,10 +4,15 @@
 #define COTE    4
 #define NbCase  (COTE^2)
 
+typedef struct Case {
+    //value est la valeur de la case de la grille
+    int value;
+    // new et fusion servent à l'affichage graphique du jeu
+    bool new;
+    bool fusion;
 
-
-
-
-
+}
+//
+typedef Case Grid[NbCase];
 
 #endif // COMMON_H
